e267872c20299fed4e5cf2f00db239c2a3f47d32
lsao2000 <lahcenenligne@gmail.com>
2023-11-19 19:26:09 +0100

    hide the api key

--- src/main/java/Constant.java
+++ src/main/java/Constant.java
@@ -2,5 +2,5 @@
  * Constant
  */
 public class Constant {
-    public static final String API_KEY = "9c0e3e1074114c3389f2f73d3ae1d733"
+    public static final String API_KEY = "9c0e3e1074114c3389f2f73d3ae1d733";
 }
